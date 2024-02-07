3b6aee46c487c9058dcd42cb947b44e8848467c4
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-02-07 07:15:36 -0300

    Add files via upload

---
+++ comando_pass.py
@@ -0,0 +1,6 @@
+number = 0
+for number in range(10):
+    if number == 5:
+        pass # Eu ainda não sei o que fazer quando o valor da variável número for igual a 5.Por este motivo vou deixar este bloco if aqui mais vou passar com pass
+    print("O número atual é: ", number)
+print("Fim do for")    
