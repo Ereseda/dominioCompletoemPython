e9862a5ed05ea7c5121e7b42cf819ccf8011a77b
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-02-07 19:05:34 -0300

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+# COLLECTION- DICTIONARY
+agencia_001 ={"Gerente da Agência": "Rodrigo Mello", "Gerente de Contas": "Katia", "Gerente de Fundos de Investimentos": "Alesandra", "Analista Financeiro": "Gustavo"}
+# print(agencia_001)
+#print(agencia_001["Gerente de Contas"])
+'''for posicao, valor in agencia_001.items():
+    print(posicao,":", valor)'''
+
+tabela = {"Codigo_agencia": "001", "Nome_agencia":"Agencia Norte", "Total_Contas": "1000", "Gerente_Agencia": agencia_001["Gerente da Agência"]}  
+print(tabela)  
