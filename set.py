d1e167a4a4117d07bd033a246cded7842866992f
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-02-07 16:22:45 -0300

    Add files via upload

---
+++ set.py
@@ -0,0 +1,11 @@
+# *** TRABALHANDO COM SET(CONJUNTO) ***
+# CRIANDO UMA COLLECTION DO TIPO SET
+
+lista_de_clientes_agência_001 = {"Eduardo", "Marcos", "Paulo", "Tatiana", "Jaqueline", "Juliana"}
+print(lista_de_clientes_agência_001)
+
+# UNIÃO DOS ELEMENTOS ENTRE DOIS SET
+
+lista_de_clientes_agência_001 = {"Eduardo", "Marcos", "Paulo", "Tatiana", "Jaqueline", "Juliana"}
+lista_de_clientes_agência_002 = {"Renato", "Jaqueline", "Eduardo", "Marcelo", "Paulo", "Tatiana"}
+print("União entre o set Agência 001 e 002", lista_de_clientes_agência_001.union(lista_de_clientes_agência_002))
