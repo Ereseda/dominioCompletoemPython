a0ba8665ce1aba041f32cf7a822095918f971dfc
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-02-07 09:18:18 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,34 @@
+# *** CRIANDO UMA LISTA VAZIA ***
+lista_de_guiche = []
+print(lista_de_guiche)
+
+# *** CRIANDO UMA LISTA COM NÚMERO DE POSIÇÕES E DADOS DEFINIDOS ***
+lista_de_guiche = [34949, 34234, 65645, 90905, 34593, "Valores alfanumérico: 123...", True, 10.50]
+print(lista_de_guiche)
+
+# *** CRIANDO  UMA LISTA COM UM NÚMERO DE POSIÇÕES DEFINIDAS, MAS SEM DEFINIR OS DADOS QUE IRÃO COMPOR CADA ***
+lista_2 =[None]*5
+lista_2 [3] = "Rodrigo Mello"
+print(lista_de_guiche)
+
+# *** OBTENDO O TAMANHO DE UMA LISTA ***
+len(lista_2)
+
+# *** INCLUINDO DADOS EM UMA LISTA ***
+lista_2 = []
+lista_2.append("Cliente Preferencial")
+print(lista_2)
+
+# *** REMOVENDO DADOS EM UMA LISTA ***
+print(lista_de_guiche)
+lista_de_guiche.remove(90905)
+print(lista_de_guiche)
+
+# *** CONCATENANDO DUAS LISTAS ***
+lista_2 =[]
+lista_2.append("Cliente Preferencial")
+lista_2 += lista_de_guiche
+print(lista_2)
+lista_3 = ["Gestantes", "Idosos"]
+lista_2 = lista_2 + lista_3
+print(lista_2)
