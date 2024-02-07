1813f9ff7e473a5610b65e2bb518db26c906969f
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-02-07 13:33:38 -0300

    Add files via upload

--- tuplas.py
+++ tuplas.py
@@ -8,3 +8,18 @@ y
 y [1]
 y[3]
 y[4]
+y[-1]
+
+x = (1,2,3,4,5,6,7,8,9,10)
+x
+x  [4:7 + 1]
+x[5:]
+x[:5]
+x[::3]
+x[2::3]
+tupla1 = ("Curso", "de")
+tupla2 = ("Programação", "em Python")
+tupla1
+tupla2
+tupla1 = tupla1 + tupla2
+tupla1
