3def0f868b378e736401c2eb96f5fe9960c685df
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-02-07 13:11:48 -0300

    Add files via upload

---
+++ tuplas.py
@@ -0,0 +1,10 @@
+# *** TRABALHANDO COM TUPLAS ***
+
+lista_de_servico = ("Investimentos", "Financiamento Habitacional", "Empréstimos")
+lista_de_servico
+
+y = (1, True, 0, 25, "AB")
+y
+y [1]
+y[3]
+y[4]
