000d731a1a3b274f0782c3b02d5af143dd1b5e89
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-02-07 14:16:55 -0300

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,9 @@
+numero_guiche_atendimento = "RK550999"
+print(numero_guiche_atendimento)
+len(numero_guiche_atendimento)
+print(numero_guiche_atendimento[1])
+
+numero_guiche_atendimento = "RK550999"
+for i in range(0,len(numero_guiche_atendimento)):
+    print(numero_guiche_atendimento[i])
+
