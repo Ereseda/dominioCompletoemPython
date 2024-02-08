68774e541fb66cc1248d0965292b1392d8b453e1
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-02-08 12:32:10 -0300

    Add files via upload

---
+++ funcao.py
@@ -0,0 +1,32 @@
+# *** QUATRO FORMAS PARA SE OBTER O RESULTADO RETORNADO POR UMA fUNÇÃO EM PYTHON
+# 1 OPÇÃO:
+def function_name(param1):
+    <comando>...
+    <comando>...
+return result 
+
+# 2 OPÇÃO:
+def function_name(param1):
+    <comando>...
+    <comando>...
+return result 
+
+vaux = function_name(param1)
+
+# 3 OPÇÃO
+def function_name(param1):
+    <comando>...
+    <comando>...
+return resul 
+
+print(function_name(param1=))
+
+
+# 4 OPÇÃO
+
+def function_name(param1):
+    <comando>...
+    <comando>...
+return result
+
+if(function_name(param1)):
